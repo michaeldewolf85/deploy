e387b0da76cdb05022db5477dd8ff0869a555db5
Michael DeWolf <michaeldewolf85@gmail.com>
2015-07-13 22:49:05 -0400

    Fix site foldernames.wq

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-drush si -y --db-url=mysql://root:@localhost/root1 --account-name=root1 --account-pass=pass --site-name=Root1 --sites-subdir=root1
-drush si -y --db-url=mysql://root:@localhost/root2 --account-name=root2 --account-pass=pass --site-name=Root2 --sites-subdir=root2
+drush si -y --db-url=mysql://root:@localhost/root1 --account-name=root1 --account-pass=pass --site-name=Root1 --sites-subdir=www.root1.com
+drush si -y --db-url=mysql://root:@localhost/root2 --account-name=root2 --account-pass=pass --site-name=Root2 --sites-subdir=www.root2.com
