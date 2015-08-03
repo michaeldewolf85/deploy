587923b23797a1b2ad64fc11b0a1340f75c2af74
Mike DeWolf <michaeldewolf85@gmail.com>
2015-08-03 15:32:15 -0400

    Update setup profile to have custom and contrib module files.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,4 @@ sudo ln -s /vagrant/root /var/www/root
 cd /vagrant/root
 drush si -y --db-url=mysql://root:@localhost/root1 --account-name=root1 --account-pass=pass --site-name=Root1 --sites-subdir=www.root1.com
 drush si -y --db-url=mysql://root:@localhost/root2 --account-name=root2 --account-pass=pass --site-name=Root2 --sites-subdir=www.root2.com
+sudo ln -s /vagrant/custom_stuff/modules /vagrant/root/sites/all/modules/custom
