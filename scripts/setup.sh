d01ba1cd07cd331803f3d735009fd86472795ea2
Mike DeWolf <michaeldewolf85@gmail.com>
2015-08-04 18:11:15 -0400

    Update dependencies and deploy configuration.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,7 @@ cd /vagrant/root
 drush si -y --db-url=mysql://root:@localhost/root1 --account-name=root1 --account-pass=pass --site-name=Root1 --sites-subdir=www.root1.com
 drush si -y --db-url=mysql://root:@localhost/root2 --account-name=root2 --account-pass=pass --site-name=Root2 --sites-subdir=www.root2.com
 sudo ln -s /vagrant/custom_stuff/modules /vagrant/root/sites/all/modules/custom
+drush --uri=www.root1.com en deploy_push -y
+drush --uri=www.root2.com en deploy_pull -y
+drush --uri=www.root1.com uli
+drush --uri=www.root2.com uli
